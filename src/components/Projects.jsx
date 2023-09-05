4918860ce3bedd53f0d5327ce3b34caf3a618b40
Khanh King <khanhnphan15@yahoo.com>
2023-09-05 03:13:04 -0400

    finalize

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import ProjectItem from './ProjectItem';
-// import Project1Img from '../assets/Project1.jpg';
-
-
+import Project1Img from '../assets/Project1.jpg'; // Import the image
+import Project2Img from '../assets/Project2.jpg'; 
+import Project3Img from '../assets/Project3.jpg'; 
+import Project4Img from '../assets/Project4.jpg'; 
 const Projects = () => {
     return (
         <div id='projects' className='max-w-[1040px] m-auto md:pl-20'>
-        <h1 className="text-4xl font-bold text-center text-[#001b5e">Projects</h1>
-        <p className="text-cebter py-8">
-            hhhhhhhhhhh
-        </p>
-        <div className="grid sm:grid-cols-2 gap-12">
-            <ProjectItem title='Hangman'/>
-            <ProjectItem title='Streamline HR'/>
-            <ProjectItem title='Music Box'/>
-            <ProjectItem title='Nota Nobile'/>
+            <h1 className="text-4xl font-bold text-center text-[#001b5e]">Projects</h1>
+            <p className="text-center py-8">
+                hhhhhhhhhhh
+            </p>
+            <div className="grid sm:grid-cols-2 gap-12">
+                {/* Pass the imported image as a prop to ProjectItem */}
+                <ProjectItem title='Hangman' img={Project1Img} />
+                <ProjectItem title='Streamline-HR' img={Project2Img} />
+                <ProjectItem title='Music Box' img={Project3Img} />
+                <ProjectItem title='Nota-Nobile' img={Project4Img} />
+            </div>
         </div>
-    </div>
     )
 }
-export default Projects
+
+export default Projects;
