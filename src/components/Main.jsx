413d6d25fe91ee3edfb13776eb16ef0579b94a52
Khanh King <khanhnphan15@yahoo.com>
2023-09-09 02:07:04 -0400

    fix width

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,7 +14,7 @@ const Main = () => {
                 alt='/'
             />
             <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
-            <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center p-[100px]'>
+            <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center p-[25px]'>
                     <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">I'm Khanh King</h1>
                     <div className="flex sm:flex-row flex-col items-center">
                         <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">I'm a
